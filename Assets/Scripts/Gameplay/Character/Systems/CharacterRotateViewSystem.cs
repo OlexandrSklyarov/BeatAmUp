50a1151c94f7cbb34aea55e2c27bc9df76565501
Sklyarov Olexandr <alexskrat@gmail.com>
2023-01-11 23:35:34 +0200

    [FIx] Change hero rotate view  system.

--- Assets/Scripts/Gameplay/Character/Systems/CharacterRotateViewSystem.cs
+++ Assets/Scripts/Gameplay/Character/Systems/CharacterRotateViewSystem.cs
@@ -16,15 +16,18 @@ namespace BT
                 
             var inputPool = world.GetPool<CharacterCommand>();
             var viewPool = world.GetPool<CharacterView>();
+            var movementPool = world.GetPool<Movement>();
 
             foreach (var e in entities)
             {
                 ref var input = ref inputPool.Get(e);
                 ref var view = ref viewPool.Get(e);
+                ref var move = ref movementPool.Get(e);
 
                 if (!input.IsMoved) continue;
 
-                var angle = Mathf.Atan2(input.Direction.x, input.Direction.z) * Mathf.Rad2Deg;
+                var dir = move.HorizontalVelocity.normalized;
+                var angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
                 var targetRotation = Quaternion.Euler(new Vector3(0f, angle, 0f));
 
                 view.ViewTransform.rotation = Quaternion.RotateTowards
